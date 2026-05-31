fc05392007b7abb34063914672a911b05401fd87
Ajay devmurari <ajaydev@yopmail.com>
2026-05-31 20:40:40 +0530

    fix bug

--- server.js
+++ server.js
@@ -101,8 +101,6 @@ io.on("connection", (socket) => {
     }
   });
 });
-l̥;
-
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
